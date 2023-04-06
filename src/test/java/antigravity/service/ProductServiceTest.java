301c2213cf7276d34abaa8a108a98dd994f1d2af
purple <iamyubora@gmail.com>
2023-04-06 16:57:13 +0900

    테스트 코드 수정

--- src/test/java/antigravity/service/ProductServiceTest.java
+++ src/test/java/antigravity/service/ProductServiceTest.java
@@ -21,6 +21,7 @@ import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
@@ -45,7 +46,7 @@ class ProductServiceTest {
     @DisplayName("상품 쿠폰 적용 정상")
     void getProductAmountTest() throws ParseException {
         // given
-        createFixture("2024-03-31");
+        createFixture(125600, "2024-03-31");
         Product product = productRepository.findAll().get(0);
         List<PromotionProducts> promotionProducts = promotionProductsRepository.findValidByProductId(product.getId());
         long[] couponIds = promotionProducts.stream().mapToLong(pp -> pp.getPromotion().getId()).toArray();
@@ -62,9 +63,7 @@ class ProductServiceTest {
         assertThat(result).isNotNull();
         assertThat(result.getOriginPrice()).isEqualTo(product.getPrice());
         assertThat(result.getDiscountPrice())
-                .isEqualTo(promotionProducts.stream()
-                        .mapToInt(pp -> productService.getDiscountPrice(pp.getProduct(), pp.getPromotion()))
-                        .sum());
+                .isEqualTo(productService.getDiscountPrice(product.getPrice(), promotionProducts));
         assertThat(result.getFinalPrice())
                 .isEqualTo(cutOffPrice(result.getOriginPrice() - result.getDiscountPrice()));
     }
@@ -73,7 +72,7 @@ class ProductServiceTest {
     @DisplayName("적용 가능한 쿠폰이 없으면 할인 가격 0원")
     void getProductAmountTest2() throws ParseException {
         // given
-        createFixture("2023-03-02"); // 기한 만료
+        createFixture(125600, "2023-03-02"); // 기한 만료
         Product product = productRepository.findAll().get(0);
         List<PromotionProducts> promotionProducts = promotionProductsRepository.findValidByProductId(product.getId());
         long[] couponIds = promotionProducts.stream().mapToLong(pp -> pp.getPromotion().getId()).toArray();
@@ -95,39 +94,46 @@ class ProductServiceTest {
     }
 
     @Test
-    @DisplayName("할인 가격 조회 정상")
+    @DisplayName("적용되는 총 할인 가격 조회 정상")
     void getDiscountPriceTest() throws ParseException {
         // given
-        createFixture("2024-03-31");
-        List<Promotion> promotions = promotionRepository.findAll();
+        createFixture(150000, "2024-03-31");
         Product product = productRepository.findAll().get(0);
+        List<PromotionProducts> promotions = promotionProductsRepository.findValidByProductId(product.getId());
 
-        for (Promotion promotion : promotions) {
-            // when
-            int result = productService.getDiscountPrice(product, promotion);
+        // when
+        int result = productService.getDiscountPrice(product.getPrice(), promotions);
 
-            //then
-            assertPrice(promotion, product, result);
-        }
+        //then
+        assertThat(result).isEqualTo(20000);
     }
 
-    private void assertPrice(Promotion promotion, Product product, int expectedPrice) {
-        PromotionType promotionType = promotion.getPromotion_type();
+    @Test
+    @DisplayName("상품가격 예외 처리 정상")
+    void getPriceTest() throws ParseException {
+        // given
+        createFixture(5000, "2024-03-31");
+        Product product = productRepository.findAll().get(0);
+        List<PromotionProducts> promotionProducts = promotionProductsRepository.findValidByProductId(product.getId());
+        long[] couponIds = promotionProducts.stream().mapToLong(pp -> pp.getPromotion().getId()).toArray();
 
-        if (promotionType.equals(PromotionType.CODE)) {
-            assertThat(expectedPrice).isEqualTo((int) (product.getPrice() * (promotion.getDiscount_value() / 100.00)));
-        } else {
-            assertThat(expectedPrice).isEqualTo(promotion.getDiscount_value());
-        }
+        ProductInfoRequest request = ProductInfoRequest.builder()
+                .productId(product.getId())
+                .couponIds(couponIds)
+                .build();
+
+        // when & then
+        String message = assertThrows(RuntimeException.class, () -> productService.getProductAmount(request)).getMessage();
+        assertThat(message).isEqualTo("상품 가격은 10,000원 이상, 10,000,000원 이하만 가능합니다.");
     }
 
-    private void createFixture(String endDt) throws ParseException {
+    private void createFixture(int originalPrice, String endDt) throws ParseException {
         System.out.println("#### FIXTURE ####");
 
         // 상품 세팅
         Product product = productRepository.save(Product.builder()
                 .name("테스트 상품")
-                .price(254900)
+                .price(originalPrice)
                 .build());
 
         // 쿠폰 세팅
